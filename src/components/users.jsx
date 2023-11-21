175283a3afd391dfb3cc0322639cd4227813d2bd
saals <salekser@gmail.com>
2023-11-21 12:53:07 +0300

    add fake.api; install bootstrap

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import api from "../api";
+
+const users = api.users.fetchAll();
 
 const Users = () => {
-  return <h1>Users</h1>;
+  return <h1>{users[0].name}</h1>;
 };
 
 export default Users;
