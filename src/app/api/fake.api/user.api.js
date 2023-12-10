d50695f1e507edb50c0645f8822002a8e376e2fb
saals <salekser@gmail.com>
2023-12-10 09:51:39 +0300

    add qualities.api.js

--- src/app/api/fake.api/user.api.js
+++ src/app/api/fake.api/user.api.js
@@ -1,33 +1,5 @@
 import { professionsObject as professions } from './profession.api'
-
-const qualities = {
-  tedious: {
-    _id: '67rdca3eeb7f6fgeed471198',
-    name: 'Нудила',
-    color: 'primary'
-  },
-  strange: {
-    _id: '67rdca3eeb7f6fgeed471100',
-    name: 'Странный',
-    color: 'secondary'
-  },
-  buller: { _id: '67rdca3eeb7f6fgeed4711012', name: 'Троль', color: 'success' },
-  alcoholic: {
-    _id: '67rdca3eeb7f6fgeed471101',
-    name: 'Алкоголик',
-    color: 'danger'
-  },
-  handsome: {
-    _id: '67rdca3eeb7f6fgeed471102',
-    name: 'Красавчик',
-    color: 'info'
-  },
-  uncertain: {
-    _id: '67rdca3eeb7f6fgeed471103',
-    name: 'Неуверенный',
-    color: 'dark'
-  }
-}
+import { qualities } from './qualities.api'
 
 const users = [
   {
