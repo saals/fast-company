b76ee602e473b2e59a6eef10c1457865f4403925
saals <salekser@gmail.com>
2023-12-22 12:10:29 +0300

    add UserCard, QualitiesCard, MeetingsCard to UserPage

--- src/app/components/pages/userPage/userPage.jsx
+++ src/app/components/pages/userPage/userPage.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { PropTypes } from 'prop-types'
-import { useHistory } from 'react-router-dom'
 
 import api from '../../../api'
-import Qualities from '../../ui/qualities'
+import UserCard from '../../ui/userCard'
+import QualitiesCard from '../../ui/qualitiesCard'
+import MeetingsCard from '../../ui/meetingsCard'
 
 const UserPage = ({ userId }) => {
   const [user, setUser] = useState()
@@ -12,40 +13,17 @@ const UserPage = ({ userId }) => {
     api.users.getById(userId).then((user) => setUser(user))
   }, [])
 
-  const history = useHistory()
-  const handleClick = () => {
-    history.push(history.location.pathname + '/edit')
-  }
-  const handleGoBack = () => {
-    history.replace('/users')
-  }
-
   if (!user) return 'Loading'
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3 shadow p-4">
-          <h1>{user.name}</h1>
-          <h3>Профессия: {user.profession.name}</h3>
-          <p>{<Qualities qualities={user.qualities} />}</p>
-          <p>Количество встреч: {user.completedMeetings}</p>
-          <h2>Рейтинг: {user.rate} / 5</h2>
-          <button
-            className="btn btn-warning fw-semibold"
-            type="button"
-            onClick={handleClick}
-          >
-            Изменить
-          </button>
+    <div className="container">
+      <div className="row gutters-sm">
+        <div className="col md-4 mb-3">
+          <UserCard user={user} />
+          <QualitiesCard data={user.qualities} />
+          <MeetingsCard value={user.completedMeetings} />
         </div>
-        <button
-          className="btn btn-info col-md-6 offset-md-3"
-          type="button"
-          onClick={handleGoBack}
-        >
-          Вернуться к списку
-        </button>
+        <div className="col md-8">Comments</div>
       </div>
     </div>
   )
