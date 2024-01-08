ed6a35408e2bd10a9f698bee75d03ee12ceaa674
saals <salekser@gmail.com>
2024-01-08 13:14:34 +0300

    create Sign Up for FireBase

--- src/app/App.js
+++ src/app/App.js
@@ -8,25 +8,27 @@ import Login from './layouts/login'
 import Users from './layouts/users'
 import ProfessionProvider from './hooks/useProfession'
 import QualityProvider from './hooks/useQuality'
+import AuthProvider from './hooks/useAuth'
 
 const App = () => {
   return (
-    <>
-      <NavBar />
-
-      <QualityProvider>
-        <ProfessionProvider>
-          <Switch>
-            <Route path="/" exact component={Main} />
-            <Route path="/login/:type?" component={Login} />
-            <Route path="/users/:userId?/:edit?" component={Users} />
-            <Redirect to="/" />
-          </Switch>
-        </ProfessionProvider>
-      </QualityProvider>
+    <div>
+      <AuthProvider>
+        <NavBar />
+        <QualityProvider>
+          <ProfessionProvider>
+            <Switch>
+              <Route path="/" exact component={Main} />
+              <Route path="/login/:type?" component={Login} />
+              <Route path="/users/:userId?/:edit?" component={Users} />
+              <Redirect to="/" />
+            </Switch>
+          </ProfessionProvider>
+        </QualityProvider>
+      </AuthProvider>
 
       <ToastContainer />
-    </>
+    </div>
   )
 }
 
